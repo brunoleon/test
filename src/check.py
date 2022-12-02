fb23f706a9cfbea5362ea2ca1833c410d7bd3c90
Bruno Léon <bruno.leon@suse.com>
2022-12-02 19:00:47 +0100

    Working prototype
    Use container to get Suse version
    Output CSV

--- src/check.py
+++ src/check.py
@@ -7,6 +7,7 @@ import logging
 import requests
 import yaml
 import subprocess
+import csv
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -29,14 +30,28 @@ def main():
     for k, v in config["projects"].items():
         logger.info(f'Processing project: {k}')
         projects[k] = Project(k, v['id'])
-        print(projects[k].get_versions())
-
+        projects[k].get_versions()
 
     c = Container("release_monitoring")
     c.create()
     for project in projects.keys():
-        c.get_version(project)
+        projects[project].suse_version = c.get_version(project)
     c.delete()
+
+    mylist = []
+    for project in projects.keys():
+        data = {
+            'package': project,
+            'upstream': projects['bmake'].version['latest_version'],
+            'suse': projects[project].suse_version
+            }
+        mylist.append(data)
+
+    with open('report.csv', 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=["package", "upstream", "suse"])
+        writer.writeheader()
+        writer.writerows(mylist)
+
 
 class Container:
     def __init__(self, name) -> None:
@@ -45,14 +60,13 @@ class Container:
     def create(self):
         subprocess.run(
             ["docker", "run", "--rm", "--name", self.name, "-d",
-            "opensuse/leap", "sleep", "60"])
-        self.run("refresh")
+            "opensuse/leap", "sleep", "1800"])
+        self.exec("refresh")
 
     def delete(self):
         subprocess.run(["docker", "rm", "-f", self.name])
-        self.run("refresh")
 
-    def run(self, cmd):
+    def exec(self, cmd):
         subprocess.run(
             ["docker", "exec", self.name, cmd],
             capture_output=True)
@@ -62,13 +76,19 @@ class Container:
             ["docker", "exec", self.name, "zypper", "-q", "info", pkg],
             capture_output=True)
         res = v.stdout.decode().split('\n')
-        version = [x for x in res if x.startswith("Version")][0].split(':')[-1].strip()
+        try:
+            version = [x for x in res if x.startswith("Version")][0].split(':')[-1].strip()
+        except:
+            logger.error(f"Unable to parse zypper output: {res}")
+            version = None
         print(version)
         return version
 
 class Project:
     def __init__(self, name, id=None) -> None:
         self.name = name
+        self.version = None
+        self.suse_version = None
         if id is None:
             self.id = self.get_project_id(self.name)
         else:
@@ -88,10 +108,13 @@ class Project:
         return result
 
     def get_versions(self):
+        if self.version is not None:
+            return self.version
         params = {
             "project_id": self.id
         }
         q = self.query_get("api/v2/versions", params)
+        self.version = q
         return q
 
     def get_version_obs(self):
@@ -116,7 +139,7 @@ class Project:
         if resp.status_code == 200:
             result = resp.json()
         else:
-            print("ERROR: Wrong arguments for request '{}'".format(resp.url))
+            logger.error("ERROR: Wrong arguments for request '{}'".format(resp.url))
         return result
 
 
