d5537efb7ab45ac98dc913e62bcada9074b6206c
Dan Čermák <dcermak@suse.com>
2023-03-20 13:46:29 +0100

    Add type hints to all functions

--- src/check.py
+++ src/check.py
@@ -4,6 +4,8 @@ Check version at http://release-monitoring.org against ours
 in OBS.
 """
 import logging
+from typing import Any, Literal, NoReturn, TypedDict, overload
+from typing_extensions import NotRequired
 import requests
 import yaml
 import subprocess
@@ -29,7 +31,50 @@ IMAGES = ['opensuse/leap:15.4', 'registry.suse.com/bci/bci-base:15.4',
           'registry.suse.com/bci/bci-base:15.3']
 
 
-def main():
+class ConfigProject(TypedDict):
+    id: None | str
+    distribution: list[str]
+    real_name: NotRequired[str]
+    suse_name: NotRequired[str]
+
+class Config(TypedDict):
+    projects: dict[str, ConfigProject]
+
+class ProjectsParams(TypedDict, total=False):
+    page: int
+    items_per_page: int
+    ecosystem: str
+    name: str
+
+class ProjectResponse(TypedDict):
+    backend: str
+    created_on: float
+    ecosystem: str
+    homepage: str
+    id: int
+    name: str
+    regex: str
+    updated_on: float
+    version: str
+    version_url: str
+    versions: list[str]
+    stable_versions: list[str]
+
+class ProjectsResponse(TypedDict):
+    items: list[ProjectResponse]
+    items_per_page: int
+    page: int
+    total_items: int
+
+class VersionsParams(TypedDict, total=False):
+    project_id: int
+
+class VersionsResponse(TypedDict):
+    latest_version: str
+    versions: list[str]
+    stable_versions: list[str]
+
+def main() -> None:
     if get_container_engine() is None:
         logger.error('You need either podman/docker installed')
         sys.exit(1)
@@ -42,7 +87,7 @@ def main():
     build_report(projects)
 
 
-def build_report(projects):
+def build_report(projects: dict[str, "Project"]) -> None:
     mylist = []
     for project in projects.keys():
         data = {
@@ -59,7 +104,7 @@ def build_report(projects):
         writer.writerows(mylist)
 
 
-def get_suse_versions(projects):
+def get_suse_versions(projects: dict[str, "Project"]) -> None:
     for image in IMAGES:
         logger.info(f'Launching {image} based container')
         c = Container("release_monitoring", image)
@@ -70,8 +115,8 @@ def get_suse_versions(projects):
                 projects[project].suse_name)
         c.delete()
 
-def gen_projects(config):
-    projects = {}
+def gen_projects(config: Config) -> dict[str, "Project"]:
+    projects: dict[str, Project] = {}
     for k, v in config["projects"].items():
         logger.info(f'Processing project: {k}')
         projects[k] = Project(k, v)
@@ -80,7 +125,7 @@ def gen_projects(config):
         time.sleep(1)
     return projects
 
-def get_container_engine():
+def get_container_engine() -> Literal['podman', 'docker'] | None:
     """Check if we are running podman or docker"""
     for engine in ['podman', 'docker']:
         if shutil.which(engine) is not None:
@@ -89,28 +134,31 @@ def get_container_engine():
     else:
         return None
 
-
 class Container:
-    def __init__(self, name, image) -> None:
-        self.name = name
-        self.engine = get_container_engine()
-        self.image = image
+    def __init__(self, name: str, image: str) -> None:
+        self.name: str = name
+        self.image: str = image
 
-    def create(self):
+        engine = get_container_engine()
+        if not engine:
+            raise RuntimeError("No container engine present")
+        self.engine = engine
+
+    def create(self) -> None:
         subprocess.run(
             [self.engine, "run", "--rm", "--name", self.name, "-d",
             self.image, "sleep", "1800"])
         self.exec("refresh")
 
-    def delete(self):
+    def delete(self) -> None:
         subprocess.run([self.engine, "rm", "-f", self.name])
 
-    def exec(self, cmd):
+    def exec(self, cmd: str) -> None:
         subprocess.run(
             [self.engine, "exec", self.name, cmd],
             capture_output=True)
 
-    def get_version(self, pkg):
+    def get_version(self, pkg: str) -> str | None:
         """Get the package version available on this container"""
         v = subprocess.run(
             [self.engine, "exec", self.name, "zypper", "-q", "info", pkg],
@@ -123,28 +171,29 @@ class Container:
             version = None
         return version
 
+
 class Project:
-    def __init__(self, name, metadata) -> None:
-        self.name = name
+    def __init__(self, name: str, metadata: ConfigProject) -> None:
+        self.name: str = name
+        self.branch: str | None = None
         if 'real_name' in metadata:
             self.real_name = metadata['real_name']
             self.branch = name.split(self.real_name)[-1]
         else:
             self.real_name = name
-            self.branch = None
         if 'suse_name' in metadata:
             self.suse_name = metadata['suse_name']
         else:
             self.suse_name = name
         self.id = self.get_project_id()
-        self.version = None
-        self.suse_versions = {}
+        self.version: str | None = None
+        self.suse_versions: dict[str, str | None] = {}
 
 
-    def get_project_id(self):
+    def get_project_id(self) -> int | None:
         """Get project id from name"""
         result = None
-        params = {
+        params: ProjectsParams = {
             "name": self.real_name
         }
         q = self.query_get("api/v2/projects", params)
@@ -152,11 +201,12 @@ class Project:
             result = q["items"][0]["id"]
         return result
 
-    def get_versions_rm(self):
+    def get_versions_rm(self) -> str | None:
         """Get package version from anitya"""
         if self.version is not None:
             return self.version
-        params = {
+
+        params: VersionsParams = {
             "project_id": self.id
         }
         q = self.query_get("api/v2/versions", params)
@@ -168,7 +218,7 @@ class Project:
                 self.version = q['stable_versions'][0]
         return self.version
 
-    def get_versions_lv(self):
+    def get_versions_lv(self) -> str | None:
         """Get package version using 'lastversion' tool"""
         if self.version is not None:
             return self.version
@@ -176,21 +226,27 @@ class Project:
         self.version = v.stdout.decode().strip()
         return self.version
 
-    def get_version_obs(self):
+    def get_version_obs(self) -> NoReturn:
         """
-        Get a project version in Suse OBS
+        Get a project version in SUSE OBS
         """
         #https://api.opensuse.org/build/openSUSE:Factory/standard/x86_64/helm
-        pass
+        raise NotImplementedError()
 
 
-    def add_to_anitya(self):
+    def add_to_anitya(self) -> NoReturn:
         """
         Add a project to release-monitoring if not present
         """
-        pass
+        raise NotImplementedError()
 
-    def query_get(self, path, params):
+    @overload
+    def query_get(self, path: str = "/api/v2/projects", params: ProjectsParams|None = None) -> ProjectsResponse: ...
+
+    @overload
+    def query_get(self, path: str = "/api/v2/versions", params: VersionsParams|None = None) -> VersionsResponse: ...
+
+    def query_get(self, path: str, params: VersionsParams | ProjectsParams | None) -> Any | None:
         result = None
         resp = requests.get(SERVER_URL + path, params=params)
         if resp.status_code == 200:
